9a1d636fbf435008979a71bd513188deebec79a3
Antoni-Stawik-PWr <280155@student.pwr.edu.pl>
2025-06-26 19:19:14 +0200

    Update hashUtils.hpp

--- hashUtils.hpp
+++ hashUtils.hpp
@@ -1,7 +1,5 @@
 ﻿#pragma once
-/*─────────────────────────────────────────────────────────────
-  wrap() – wynik % m przenosi do przedziału 0‥m-1  (obsługa <0)
-─────────────────────────────────────────────────────────────*/
+
 inline int wrap(int h, int m)
 {
     return ((h % m) + m) % m;
